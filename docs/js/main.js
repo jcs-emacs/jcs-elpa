e5169a5ea2edab5ad7130c58186195a75e4b6309
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-22 23:58:17 +0800

    Add js

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * $File: main.js $
+ * $Date: 2022-01-22 23:53:52 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+
+"use strict";
