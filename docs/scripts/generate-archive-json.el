39d8a68810b8a4bbcebdf2c7ffdf5d58d1aae58a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 03:16:07 +0800

    Update

--- docs/scripts/generate-archive-json.el
+++ docs/scripts/generate-archive-json.el
@@ -32,11 +32,7 @@
       (push (cons "version" version) object)
       (push (cons "url" url) object)
       (push object json)))
-  (write-region (json-encode json) nil "./docs/archive.json")
-  (message "json: %s" (with-temp-buffer
-                        (insert-file-contents "./docs/archive.json")
-                        (buffer-string)))
-  )
+  (write-region (json-encode json) nil "./archive.json"))
 
 ;; Local Variables:
 ;; coding: utf-8
