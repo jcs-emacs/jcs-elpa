190d066b2a9b706e7940a808f3774288d0673a89
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 00:28:35 +0800

    Try remove bottom

--- docs/scripts/generate-archive-json.el
+++ docs/scripts/generate-archive-json.el
@@ -18,21 +18,6 @@
 
 (pop archive-contents)  ; remove 1
 
-(let (json)
-  (dolist (pkg archive-contents)
-    (let* ((pkg-name (car pkg)) (desc (cdr pkg))
-           (version (aref desc 0))
-           (summary (aref desc 2))
-           (extras (aref desc 4))
-           (url (cdr (assq :url extras)))
-           object)
-      (push (cons "name" pkg-name) object)
-      (push (cons "summary" summary) object)
-      (push (cons "version" version) object)
-      (push (cons "url" url) object)
-      (push object json)))
-  (write-region (json-encode json) nil "./archive.json"))
-
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
