1068669314c5e23b9ca4a90ed891f43dc8043656
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 19:11:07 +0800

    Mis prnit

--- bin/generate-badges.el
+++ bin/generate-badges.el
@@ -14,13 +14,13 @@
 
 (ignore-errors (delete-directory output-dir t))  ; clean up first
 
-(jcs-print "mkdir!")
+(message "mkdir!")
 (make-directory output-dir)
 
-(jcs-print "?")
+(message "?")
 
 (dolist (pkg archive-contents)
-  (jcs-print pkg)
+  (message "%s" pkg)
   (let* ((pkg-name (car pkg)) (desc (cdr pkg))
          (version (aref desc 0))
          (version (mapconcat (lambda (item) (format "%s" item)) version "."))
