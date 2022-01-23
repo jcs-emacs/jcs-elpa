2753338ebffbca04024caa322540a61c23917764
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 19:09:26 +0800

    Test

--- bin/generate-badges.el
+++ bin/generate-badges.el
@@ -13,11 +13,14 @@
   "Where the badges store.")
 
 (ignore-errors (delete-directory output-dir t))  ; clean up first
+
+(jcs-print "mkdir!")
 (make-directory output-dir)
 
-
+(jcs-print "?")
 
 (dolist (pkg archive-contents)
+  (jcs-print pkg)
   (let* ((pkg-name (car pkg)) (desc (cdr pkg))
          (version (aref desc 0))
          (version (mapconcat (lambda (item) (format "%s" item)) version "."))
