76ff6c3938a2544a021b1ca5be71f40187470146
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 21:19:02 +0800

    Update lsit

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,16 +9,25 @@
 
 "use strict";
 
-const archive_url = "https://raw.githubusercontent.com/jcs-emacs/elpa/master/docs/archive.json";
-var archive = null;
+const base_url = "https://raw.githubusercontent.com/jcs-emacs/jcs-elpa/";
+const archive_url = base_url + "master/docs/archive.json";
 
 $(document).ready(function(){
+  var packageList = $('#package-list');
 
-  $.getJSON(archive_url, function(data){
-    archive = data;
-    console.log(data);
+  $.getJSON(archive_url, function(archive){
+    for (let index = 0; index < archive.length; ++index) {
+      let desc = archive[index];
+      packageList.append(
+        '<tr>' +
+          '<td>' + desc.name + '</td>' +
+          '<td>' + desc.summary + '</td>' +
+          '<td>' + desc.version + '</td>' +
+          '<td>' + 'github' + '</td>' +
+          '<td><img src="' + base_url + 'master/badges/' + desc.name + '.svg"/></td>' +
+          '</tr>');
+    }
   }).fail(function(){
     console.log("An error has occurred.");
   });
-
 });
