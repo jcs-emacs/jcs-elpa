dd93901042acab3ca6c9164eb226300095e8c3ba
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 22:38:52 +0800

    Add search capability

--- docs/js/main.js
+++ docs/js/main.js
@@ -13,23 +13,46 @@ const base_url = "https://github.com/jcs-emacs/jcs-elpa/blob/master/";
 const base_url_raw = "https://raw.githubusercontent.com/jcs-emacs/jcs-elpa/";
 const archive_url = base_url_raw + "master/docs/archive.json";
 
+var archive = null;
+
 $(document).ready(function(){
   var packageList = $('#package-list');
+  var filter = $('#filter');
 
-  $.getJSON(archive_url, function(archive){
+  $.getJSON(archive_url, function(data){
+    archive = data;
+    disply('');
+  }).fail(function(){
+    console.log("An error has occurred.");
+  });
+
+  filter.on('input', function() {
+    disply(filter.val());
+  });
+
+  function filtering(query, desc) {
+    if (query === '' ||
+        desc.name.includes(query) ||
+        desc.summary.includes(query))
+      return true;
+    return false;
+  }
+
+  function disply(query) {
+    packageList.empty();
     for (let index = 0; index < archive.length; ++index) {
       let desc = archive[index];
+      if (!filtering(query, desc))
+        continue;
       packageList.append(
         '<tr>' +
-          '<td>' + desc.name + '</td>' +
-          '<td>' + desc.summary + '</td>' +
+          '<td><a href="' + desc.url + '">' + desc.name + '</a></td>' +
+          '<td><a href="' + desc.url + '">' + desc.summary + '</a></td>' +
           '<td>' + desc.version + '⭳</td>' +
           '<td><a href="' + base_url + 'recipes/' + desc.name + '">🍴</a></td>' +
           '<td><a href="' + desc.url + '">' + desc.source + '</a></td>' +
           '<td><img src="' + base_url_raw + 'master/badges/' + desc.name + '.svg"/></td>' +
           '</tr>');
     }
-  }).fail(function(){
-    console.log("An error has occurred.");
-  });
+  }
 });
