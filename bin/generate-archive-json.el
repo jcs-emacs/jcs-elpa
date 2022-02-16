20171a817a7c316152bba2f859c8b1eea0e1eae2
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-17 02:02:06 +0800

    Convert to string

--- bin/generate-archive-json.el
+++ bin/generate-archive-json.el
@@ -29,8 +29,7 @@
 (defun construct-url (name source)
   "Construct the url by SOURCE."
   (when-let ((repo (get-repo name)))
-    (message ">> %s" (type-of source))
-    (pcase source
+    (pcase (format "%s" source)
       ("github" (concat "https://github.com/" repo))
       ("gitlab" (concat "https://gitlab.com/" repo)))))
 
