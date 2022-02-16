494b07ea374b943531b65ca522a8de6dcd7cdabe
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-17 00:40:42 +0800

    Test

--- bin/generate-archive-json.el
+++ bin/generate-archive-json.el
@@ -11,6 +11,8 @@
   "Return tree url."
   (if (member source supported-source) (concat url "/tree/" commit) url))
 
+(message "====================================")
+
 (let (json)
   (dolist (pkg archive-contents)
     (let* ((pkg-name (car pkg)) (desc (cdr pkg))
