08d7880f2818f982b371819104d8d706bb774c14
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-17 01:25:27 +0800

    Ensure url

--- bin/generate-archive-json.el
+++ bin/generate-archive-json.el
@@ -10,13 +10,20 @@
     ((or "github" "gitlab") (concat url "/tree/" commit))
     (_ url)))
 
-(defun get-fetcher (name)
-  "Get fetcher."
+(defun recipe-data (name)
+  "Return recipe data."
   (when-let* ((file (format "./recipes/%s" name))
               (content (file-to-string file))
               (data (eval (thing-at-point--read-from-whole-string (concat "'" content)))))
-    (pop data)  ; remove name
-    (plist-get data :fetcher)))
+    (pop data)
+    data))
+
+(defun recipe-get (name prop)
+  "Return recipe data."
+  (when-let ((data (recipe-data name))) (plist-get data prop)))
+
+(defun get-fetcher (name) "Get fetcher." (recipe-get :fetcher))
+(defun get-url (name) "Get url." (recipe-get :url))
 
 (let (json)
   (dolist (pkg archive-contents)
@@ -24,7 +31,7 @@
            (version (package-version-join (aref desc 0)))
            (summary (aref desc 2))
            (extras (aref desc 4))
-           (url (or (cdr (assq :url extras)) ""))
+           (url (or (cdr (assq :url extras)) (get-url pkg-name) ""))
            (commit (cdr (assq :commit extras)))
            (source (get-fetcher pkg-name))
            (tree (tree-url source url commit))
