f0ae2b55c62569f0d970af32333c7dfc8af99027
Jen-Chieh <jcs090218@gmail.com>
2022-07-04 13:57:40 +0800

    test

--- scripts/build.el
+++ scripts/build.el
@@ -8,7 +8,21 @@
       github-elpa-archive-dir "./docs/packages"
       github-elpa-recipes-dir "./recipes")
 
-(github-elpa-build)
+(let ((package-build-working-dir (expand-file-name github-elpa-working-dir))
+      (package-build-archive-dir (expand-file-name github-elpa-archive-dir))
+      (package-build-recipes-dir (expand-file-name github-elpa-recipes-dir)))
+  ;;(github-elpa--git-check-repo)
+  ;;(github-elpa--git-check-workdir-clean)
+  (make-directory package-build-archive-dir t)
+  ;; Currently no way to detect build failure...
+  (dolist (recipe (directory-files package-build-recipes-dir nil "^[^.]"))
+    (message "")
+    (message "")
+    (message ":: github-elpa: packaging recipe %s" recipe)
+    (let ((package-build-tar-executable (or github-elpa-tar-executable
+                                            package-build-tar-executable)))
+      (package-build-archive recipe)))
+  (package-build-cleanup))
 
 ;; Local Variables:
 ;; coding: utf-8
