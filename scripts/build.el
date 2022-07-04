96628d3fd244b92314d4b6fe56ef33f62c272c4f
Jen-Chieh <jcs090218@gmail.com>
2022-07-04 13:39:10 +0800

    test error

--- scripts/build.el
+++ scripts/build.el
@@ -21,7 +21,7 @@
     (message ":: github-elpa: packaging recipe %s" recipe)
     (let ((package-build-tar-executable (or github-elpa-tar-executable
                                             package-build-tar-executable)))
-      (ignore-errors (package-build-archive recipe))))
+      (package-build-archive recipe)))
   (package-build-cleanup))
 
 ;; Local Variables:
