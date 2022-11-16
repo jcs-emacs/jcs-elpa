57496016847f39060a9b4634e7832084d1eab121
Jen-Chieh Shen <jcs090218@gmail.com>
2022-11-17 01:11:31 +0800

    more

---
+++ scripts/build-archive-contents.el
@@ -0,0 +1,14 @@
+;;; build-archive-contents.el --- Build archive-contents file  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(load-file "./scripts/_prepare.el")
+
+(with-package-build-env
+  (package-build-cleanup))
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; build-archive-contents.el ends here
