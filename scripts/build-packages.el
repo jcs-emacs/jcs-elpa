59724e77e2735ed2fe324fadb72f8d53571802c3
Jen-Chieh Shen <jcs090218@gmail.com>
2022-11-17 18:12:31 +0800

    Extract section var

--- scripts/build-packages.el
+++ scripts/build-packages.el
@@ -4,25 +4,27 @@
 
 (load-file "./scripts/_prepare.el")
 
-(defconst packages-per-section 50
+(defconst per-section (string-to-number (getenv "PER_SECTION"))
   "Build this many packages in one section.")
 
-(defun recipe-in-section-p (count total current)
+(defconst elpa-section (string-to-number (getenv "ELPA_SECTION"))
+  "Section ID.")
+
+(defun recipe-in-section-p (count total)
   "Return t only when the recipe is in the right section."
-  (let ((min (1+ (* packages-per-section (1- current))))
-        (max (* packages-per-section current)))
+  (let ((min (1+ (* per-section (1- elpa-section))))
+        (max (* per-section elpa-section)))
     (and (<= min count) (<= count max))))
 
 (with-package-build-env
   (make-directory package-build-archive-dir t)
   (let* ((recipes (directory-files package-build-recipes-dir nil "^[^.]"))
-         (total (ceiling (/ (float (length recipes)) (float packages-per-section))))
-         (current (string-to-number (getenv "ELPA_SECTION")))
+         (total (ceiling (/ (float (length recipes)) (float per-section))))
          (count 0))
-    (message "BUILD SECTION: %s" current)
+    (message "BUILD SECTION: %s" elpa-section)
     (dolist (recipe recipes)
       (cl-incf count)
-      (when (recipe-in-section-p count total current)
+      (when (recipe-in-section-p count total)
         (message "")
         (message "")
         (message ":: github-elpa: packaging recipe %s" recipe)
