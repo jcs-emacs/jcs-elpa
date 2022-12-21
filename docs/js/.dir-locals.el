627d02970cfed1ae69f14ee3aed40032f8e8af39
Jen-Chieh <jcs090218@gmail.com>
2022-12-21 13:27:50 +0800

    Add dir-locals

---
+++ docs/js/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-mode . ((lsp-enabled-clients . (ts-ls))))
+ (markdown-mode . ((fill-column . 80))))
