db73f5b3c38e6a8d82fb1505a71ec82b8d5ac928
Jen-Chieh Shen <jcs090218@gmail.com>
2023-01-02 14:45:11 +0800

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((js-mode . ((lsp-enabled-clients . (ts-ls))))
- (markdown-mode . ((fill-column . 80))))
+((js-mode       . ((lsp-enabled-clients . (ts-ls))))
+ (markdown-mode . ((fill-column         . 80))))
